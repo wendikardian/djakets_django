52660c7d4e1195f3245ed608d0181118f6c87b48
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-03-10 16:31:00 +0700

    updated with a increment and decrement item

--- djackerts_vue/src/components/CartItem.vue
+++ djackerts_vue/src/components/CartItem.vue
@@ -10,12 +10,14 @@
         </td>
         <td>
             {{ item.quantity }}
+            <a @click="decrementQuantity(item)">-</a>
+            <a @click="incrementQuantity(item)">+</a>
         </td>
         <td>
             ${{ getItemTotal(item).toFixed(2)  }}
         </td>
         <td>
-            <button class="delete" ></button>
+            <button class="delete" @click="removeFromCart(item)" ></button>
         </td>
     </tr>
 </template>
@@ -33,6 +35,21 @@ export default {
   },methods : {
     getItemTotal(item){
         return item.quantity * item.product.price
+    }, decrementQuantity(item){
+        item.quantity -= 1
+        if(item.quantity === 0){
+            this.$emit('removeFromCart', item)
+        }
+        this.updateChart()
+
+    }, incrementQuantity(item){
+        item.quantity += 1
+        this.updateChart()
+    }, updateChart(){
+        localStorage.setItem('cart', JSON.stringify(this.$store.state.cart))
+    }, removeFromCart(item){
+        this.$emit('removeFromCart', item)
+        this.updateChart()
     }
   }
 };
--- djackerts_vue/src/views/Cart.vue
+++ djackerts_vue/src/views/Cart.vue
@@ -20,6 +20,7 @@
               v-for="item in cart.items"
               v-bind:key="item.product.id"
               v-bind:initialItem="item"
+              v-on:removeFromCart="removeFromCart"
             />
           </tbody>
         </table>
@@ -58,6 +59,11 @@ export default {
   mounted() {
     this.cart = this.$store.state.cart;
   },
+  methods : {
+    removeFromCart(item){
+        this.cart.items = this.cart.items.filter(i => i.product.id !== item.product.id)
+    }
+  } ,
   computed: {
     cartTotalLength() {
       return this.cart.items.reduce((acc, curVal) => {
