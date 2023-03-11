4c25a13f9f410c9f9edcf60d5381a6767fe4ba70
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-03-11 12:08:15 +0700

    untill create a checkout function atleast, next add some success for checkout

--- djackerts_vue/src/views/Checkout.vue
+++ djackerts_vue/src/views/Checkout.vue
@@ -19,7 +19,7 @@
               <td>{{ item.product.name }}</td>
               <td>{{ item.product.price }}</td>
               <td>{{ item.quantity }}</td>
-              <td>${{ getItemTotal(item) }}.toFixed(2)</td>
+              <td>${{ getItemTotal(item).toFixed(2) }}</td>
             </tr>
           </tbody>
           <tfoot>
@@ -30,6 +30,81 @@
             </tr>
           </tfoot>
         </table>
+      </div>
+      <div class="column is-12 box">
+        <h2 class="subtitle">Shipping Details</h2>
+        <p class="has-text-grey mb-4">*All Fields are required</p>
+
+        <div class="columns is-multiline">
+          <div class="column is-6">
+            <div class="field">
+              <label>First name*</label>
+              <div class="control">
+                <input type="text" class="input" v-model="first_name" />
+              </div>
+            </div>
+
+            <div class="field">
+              <label>Last name*</label>
+              <div class="control">
+                <input type="text" class="input" v-model="last_name" />
+              </div>
+            </div>
+
+            <div class="field">
+              <label>E-mail*</label>
+              <div class="control">
+                <input type="email" class="input" v-model="email" />
+              </div>
+            </div>
+
+            <div class="field">
+              <label>Phone*</label>
+              <div class="control">
+                <input type="text" class="input" v-model="phone" />
+              </div>
+            </div>
+          </div>
+
+          <div class="column is-6">
+            <div class="field">
+              <label>Address*</label>
+              <div class="control">
+                <input type="text" class="input" v-model="address" />
+              </div>
+            </div>
+
+            <div class="field">
+              <label>Zip code*</label>
+              <div class="control">
+                <input type="text" class="input" v-model="zipcode" />
+              </div>
+            </div>
+
+            <div class="field">
+              <label>Place*</label>
+              <div class="control">
+                <input type="text" class="input" v-model="place" />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="notification is-danger mt-4" v-if="errors.length">
+          <p v-for="error in errors" v-bind:key="error">{{ error }}</p>
+        </div>
+
+        <hr />
+
+        <div id="card-element" class="mb-5"></div>
+
+        <template v-if="cartTotalLength">
+          <hr />
+
+          <button class="button is-dark" @click="submitForm">
+            Pay with Stripe
+          </button>
+        </template>
       </div>
     </div>
   </div>
@@ -66,11 +141,35 @@ export default {
     getItemTotal(item) {
       return item.quantity * item.product.price;
     },
+    submitForm() {
+      this.errors = [];
+      if (this.first_name === "") {
+        this.errors.push("The first name field is missing!");
+      }
+      if (this.last_name === "") {
+        this.errors.push("The last name field is missing!");
+      }
+      if (this.email === "") {
+        this.errors.push("The email field is missing!");
+      }
+      if (this.phone === "") {
+        this.errors.push("The phone field is missing!");
+      }
+      if (this.address === "") {
+        this.errors.push("The address field is missing!");
+      }
+      if (this.zipcode === "") {
+        this.errors.push("The zip code field is missing!");
+      }
+      if (this.place === "") {
+        this.errors.push("The place field is missing!");
+      }
+    },
   },
   computed: {
     cartTotalPrice() {
       return this.cart.items.reduce((acc, curVal) => {
-        return (acc += curlVal.product.price * curVal.quantity);
+        return (acc += curVal.product.price * curVal.quantity);
       }, 0);
     },
     cartTotalLength() {
