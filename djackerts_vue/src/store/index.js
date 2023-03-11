b38410cd05bdee866792cac3bf549319673d835e
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-03-11 11:12:20 +0700

    update data

--- djackerts_vue/src/store/index.js
+++ djackerts_vue/src/store/index.js
@@ -1,40 +1,55 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    cart : {
+    cart: {
       items: [],
     },
     isAuthenticated: false,
-    token : '',
+    token: "",
     isLoading: false,
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
     initializeStore(state) {
-      if (localStorage.getItem('cart')) {
-        state.cart = JSON.parse(localStorage.getItem('cart'))
-      }else{
-        localStorage.setItem('cart', JSON.stringify(state.cart)) 
-      }
-    },
-    addToCart(state, item){
-      const exists = state.cart.items.filter(i => i.product.id === item.product.id)
-      if(exists.length){
-        exists[0].quantity = parseInt(exists[0].quantity + parseInt(item.quantity))
-      }else{
-        state.cart.items.push(item)
+      if (localStorage.getItem("cart")) {
+        state.cart = JSON.parse(localStorage.getItem("cart"));
+      } else {
+        localStorage.setItem("cart", JSON.stringify(state.cart));
       }
 
-      localStorage.setItem('cart', JSON.stringify(state.cart))
+      if(localStorage.getItem('token')){
+        state.token = localStorage.getItem('token')
+        state.isAuthenticated = true
+      }else {
+        state.token = ''
+        state.isAuthenticated = false
+      }
     },
-    setIsLoading(state, status){
-      state.isLoading = status
+    addToCart(state, item) {
+      const exists = state.cart.items.filter(
+        (i) => i.product.id === item.product.id
+      );
+      if (exists.length) {
+        exists[0].quantity = parseInt(
+          exists[0].quantity + parseInt(item.quantity)
+        );
+      } else {
+        state.cart.items.push(item);
+      }
+
+      localStorage.setItem("cart", JSON.stringify(state.cart));
+    },
+    setIsLoading(state, status) {
+      state.isLoading = status;
+    },setToken(state, token) {
+      state.token = token
+      state.isAuthenticated = true
+    }, removeToken(state){
+      state.token = ''
+      state.isAuthenticated = false
     }
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
